ca23d1ce130aa3b48bf9fd465c4c0e0f530b8ccf
Hanzala Arham <144531149+unknow2002@users.noreply.github.com>
2024-10-28 20:49:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,3 @@ movies = movie_titles[::-1]
 with open("movies.txt", mode="w") as file:
     for movie in movies:
         file.write(f"{movie}\n")
-
-
-'''
-FAQ: Empire's website has changed!
-
-When this lesson was created, I used this URL for the project: 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
-
-However, Empire has since changed their website. You can see this when you inspect the movie title elements. You'll 
-see that the h3 with the class "title" is no longer there. To use exactly the same code as per the solution, 
-we can use a cached version of the website from the Internet Archive's Wayback Machine.
-
-'''
